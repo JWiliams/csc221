972d2a7dd8cf10dfaf657689a39bcd1ada9ec68e
Javian Williams <javianwilliams@students.belhaven.edu>
2020-10-03 11:58:06 -0500

    homework 4

---
+++ hw4/part3/table_printer.py
@@ -0,0 +1,22 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(table):
+    # create a new list of 3 "0" values: one for each list in tableData
+    colWidths = [0] * len(table)
+    # search for the longest element in each array of 
+    for arrays in range(len(table)):
+        for elements in table[arrays]:
+            if colWidths[arrays] < len(elements):
+                colWidths[arrays] = len(elements)
+
+    # forming elements into table
+    for elements in range(len(table[0])) :
+        for arrays in range(len(table)) :
+            print(table[arrays][elements].rjust(colWidths[arrays]), end=" ")
+        print()
+        elements += 1
+
+printTable(tableData)
+
